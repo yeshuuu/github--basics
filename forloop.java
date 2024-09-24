3abe53ae91e2c3f6689d87955e7bede59344b852
yeshu <pyeshuu@gmail.com>
2024-09-24 13:54:46 -0700

    Basic java program on forloop

---
+++ forloop.java
@@ -0,0 +1,13 @@
+public class forloop {
+    public static void main(String[] args) {
+
+        //Display Numbers from 1 to 5
+        int n = 5;        
+        for ( int i = 1; i <=n; i++) {
+            System.out.println(i);
+            
+        }
+
+    }
+    
+}
