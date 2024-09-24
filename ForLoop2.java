afd755d8061cb4f424bb9d711a84fb22cbd64bec
yeshu <pyeshuu@gmail.com>
2024-09-24 14:32:01 -0700

    second program on ForLoop

---
+++ ForLoop2.java
@@ -0,0 +1,16 @@
+ // Display sum of n Natural Numbers
+public class ForLoop2 {
+    public static void main (String args[])
+    {
+        int sum=0, n=5;
+        for(int i = 0; i <= n; i++) {
+            sum =sum + i;
+            
+        }
+        System.out.println("The sum of N natural Numbers are :" + sum);
+
+       
+
+    }
+    
+}
