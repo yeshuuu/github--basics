e20f2c2071b48a8af1ad8705ac2d49d8d5efb1a4
yeshu <pyeshuu@gmail.com>
2024-09-26 16:19:15 -0700

    Commiting 2 basic programs on While Loop

---
+++ WhileLoop.java
@@ -0,0 +1,28 @@
+public class WhileLoop {
+
+    //Print the numbers which are divisible by 7 from no's 150 to 200.
+    public static void main(String[] args)
+    /*/ {
+        int i = 150;
+        int j = 200;
+        while (i<=j)
+        {
+            if (i%7==0)
+            System.out.println(i);
+            i++;
+
+        }
+        
+    }*/
+
+    //Print Even No's between 200 to 500
+    {
+        int i= 200, j =500;
+        while (i<=j){
+            if(i%2==0)
+            System.out.println(i);
+            i++;
+        }
+
+    }
+}
