9cc1efdc665f1c6a9c7429091a27a20b59f4bdf6
yeshu <pyeshuu@gmail.com>
2024-09-27 17:44:43 -0700

    Commiting return type int of add of 2 numbers

---
+++ ReturnAdd.java
@@ -0,0 +1,20 @@
+public class ReturnAdd{
+
+    public int add(int a, int b) {
+
+        return (a + b);
+    }
+    
+    public static void main(String args[]){
+        ReturnAdd r = new ReturnAdd();
+        int n = r.add(3, 8);
+        System.out.println(n);
+
+
+    }
+
+    
+}
+
+
+    
