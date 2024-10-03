6fa4543ea56f3b41028370d9a6b9215f4ce19d12
yeshu <pyeshuu@gmail.com>
2024-10-03 13:39:07 -0700

    String Buffer

---
+++ Strbuffer.java
@@ -0,0 +1,8 @@
+public class Strbuffer {
+        public static void main (String args[]){
+            StringBuffer sb = new StringBuffer("Dolly");
+            System.out.println(sb.capacity());
+            System.out.println(sb.length());
+        }
+    }
+    
